2542e80d4763ffebb2ee767797189e0e6ef46e8d
Justice Suh <justice.suh@gmail.com>
2019-07-20 18:14:35 -0400

    Parse enums

--- gen/genopengl.py
+++ gen/genopengl.py
@@ -13,15 +13,26 @@ class Group():
         return self.name
 
 
+class Enum():
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return self.name
+
+
 class Registry():
     def __init__(self, url):
         self.url = url
         self.filename = url.rsplit('/', 1)[-1]
         self.groups = []
+        self.enums = []
 
     def download(self):
         if os.path.exists(self.filename):
             return
+
         with open(self.filename, 'wb') as f:
             r = requests.get(self.url)
             f.write(r.content)
@@ -31,10 +42,10 @@ class Registry():
             xml = f.read()
         root = etree.fromstring(xml.encode('utf-8'))
 
-        comments = root.xpath('//comment()')
-        for c in comments:
-            p = c.getparent()
-            p.remove(c)
+        for c in root.xpath('//comment()'):
+            c.getparent().remove(c)
+        for u in root.xpath('//unused'):
+            u.getparent().remove(u)
 
         for element in root.getchildren():
             if element.tag == 'groups':
@@ -43,6 +54,10 @@ class Registry():
                         group.attrib['name'],
                         [enum.attrib['name'] for enum in group.getchildren()]
                     ))
+
+            if element.tag == 'enums':
+                for enum in element.getchildren():
+                    self.enums.append(Enum(enum.attrib['name'], enum.attrib['value']))
 
 
 if __name__ == '__main__':
