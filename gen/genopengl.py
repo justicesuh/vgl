9ce8f7061833a6b17fd956bdc2fc8e014aeabb52
Justice Suh <justice.suh@gmail.com>
2019-07-20 17:12:43 -0400

    Use xml url

--- gen/genopengl.py
+++ gen/genopengl.py
@@ -1,5 +1,6 @@
 import os
 
+import lxml
 import requests
 
 
@@ -7,9 +8,14 @@ def download_registry():
     if os.path.exists('gl.xml'):
         return
     with open('gl.xml', 'wb') as f:
-        r = requests.get('https://github.com/KhronosGroup/OpenGL-Registry/blob/master/xml/gl.xml')
+        r = requests.get('https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml')
         f.write(r.content)
+
+
+def parse_registry():
+    pass
 
 
 if __name__ == '__main__':
     download_registry()
+    parse_registry()
