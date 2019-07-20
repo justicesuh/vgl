27750740eb41cb378e1a57764ce7ce30d6fd7c00
Justice Suh <justice.suh@gmail.com>
2019-07-20 17:54:10 -0400

    Get top level xml tags

--- gen/genopengl.py
+++ gen/genopengl.py
@@ -1,21 +1,33 @@
 import os
 
-import lxml
 import requests
+from lxml import etree
 
 
-def download_registry():
-    if os.path.exists('gl.xml'):
-        return
-    with open('gl.xml', 'wb') as f:
-        r = requests.get('https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml')
-        f.write(r.content)
+class Registry():
+    def __init__(self, url):
+        self.url = url
+        self.filename = url.rsplit('/', 1)[-1]
 
+    def download(self):
+        if os.path.exists(self.filename):
+            return
+        with open(self.filename, 'wb') as f:
+            r = requests.get(self.url)
+            f.write(r.content)
 
-def parse_registry():
-    pass
+    def parse(self):
+        with open(self.filename) as f:
+            xml = f.read()
+        root = etree.fromstring(xml.encode('utf-8'))
+
+        for element in root.getchildren():
+            if element.tag == etree.Comment or element.tag == 'comment':
+                continue
+            print(element.tag)
 
 
 if __name__ == '__main__':
-    download_registry()
-    parse_registry()
+    registry = Registry('https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml')
+    registry.download()
+    registry.parse()
